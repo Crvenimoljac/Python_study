89361acb51a46ddc0df17a1d54e66b6d5244cb4a
crvenimoljac <moljaccrveni@gmail.com>
2024-10-10 19:09:24 +0200

    Zadatak 16

--- domaci/d14.py
+++ domaci/d14.py
@@ -1,10 +1,10 @@
 prosek=float(input("Unesi svoj prosek ocena: "))
 
-if prosek >9:
+if prosek >=9:
     print("Tvoj prosek je odlican")
-elif prosek>7 or prosek <8:
+elif prosek>=7 and prosek <9:
     print("Tvoj prosek je vrlo dobar")
-elif prosek>5 or prosek <6:
+elif prosek>5 and prosek <7:
     print("Tvoj prosek je dobar")
-elif prosek <5:
-    print("Tvoj prosek je odlican")
+elif prosek <=5:
+    print("Tvoj prosek je nedovoljan")
