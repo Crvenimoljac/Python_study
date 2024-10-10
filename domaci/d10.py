b4ffaacd95f3ecbef98262bea44cfa833fef9581
crvenimoljac <moljaccrveni@gmail.com>
2024-10-10 16:53:02 +0200

    Zadatak 11

---
+++ domaci/d10.py
@@ -0,0 +1,4 @@
+b1=int(input("Unesi prvi izraz(0 za false ili 1 za true): "))
+b2=int(input("Unesi drui izraz(0 za false ili 1 za true): "))
+r= b1 and b2
+print(r)
--- domaci/d9.py
+++ domaci/d9.py
@@ -1,3 +1,4 @@
-E=float(input("Unesi vrednost u evrima: "))
-D=round(E*1.1, 1)
-print(f"{E} je {D} dolara.")
+E=round(float(input("Unesi vrednost u evrima: ")),2)
+k=round(float(input("Unesi danasnji kurs: ")),2)
+D=round(E*k, 1)
+print(f"{E} evra je {D} dolara.")
