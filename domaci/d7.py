a30d51e037190e1407a4906f7612cea3f4ba8b69
crvenimoljac <moljaccrveni@gmail.com>
2024-10-10 15:37:51 +0200

    Zadatak 7

---
+++ domaci/d7.py
@@ -0,0 +1,5 @@
+print("Konvertuj iz C u K i F.")
+C=int(input("Unesi temperaturu u C: "))
+F=(C*9/5)+32
+K=C+273.15
+print(f"Temperatura u Celzijusima je:{C}\nTemperatura u farenhajtima je:{F}\nTemperatura u kelvinima je:{K}")
