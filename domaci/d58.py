5079ae492a6988fff86f20b9a3968117d627bf47
ika1994 <veselinovic.ilija250@gmail.com>
2024-10-15 22:33:40 +0200

    ispravljen fibonacci

--- domaci/d58.py
+++ domaci/d58.py
@@ -3,6 +3,7 @@
 # Program zatim traži od korisnika da unese broj, 
 # poziva funkciju i ispisuje rezultat.
 # Svaki novi član niza se dobija kao zbir prethodna 2.
+
 def fibonacijev_niz(n:int):
     a,b=0,1
     r=[]
@@ -10,5 +11,16 @@ def fibonacijev_niz(n:int):
         a,b=b,a+b
         r.append(a)
     return r
+
+#Ispravljen kod
+def fibonacci(n:int)->list:
+    arr=[0,1]
+    for i in range(2,n):
+        arr.append(arr[-1]+arr[-2])
+        if(arr[i]>n):
+            break
+    return arr
+
 a=int(input("Unesi broj do kog zelis da izracunas fibonacijev niz: \n"))
 print(f"Fibonacijev niz do {a} broja je:{fibonacijev_niz(a)} ")
+print(f"\n\nFibonacijev niz do {a} broja je: {fibonacci(a)}")
